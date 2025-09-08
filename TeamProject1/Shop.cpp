cf7ec0b64426f55b8925970be11bf4942377302e
Umsangmin <osm0991@naver.com>
2025-09-08 12:50:02 +0900

    소문자 함수를 대문자 함수로 변경

--- TeamProject1/Shop.cpp
+++ TeamProject1/Shop.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include "Character.h"
 #include "Item.h"
+#include "Inventory.h"
 
 
 void Shop::BuyItem(Item* item, Character* character, Inventory* inventory)
 {
-	if (character->Getgold() >= item->GetPrice())	//골드가 충분하면
+	if (character->GetGold() >= item->GetPrice())	//골드가 충분하면
 	{
-		character->Setgold(character->Getgold() - item->GetPrice());	//골드 차감, 현재골드 - 아이템 가격 = 남는 골드
+		character->SetGold(character->GetGold() - item->GetPrice());	//골드 차감, 현재골드 - 아이템 가격 = 남는 골드
 		inventory->AddItem(item);					//인벤토리에 아이템 추가 
 		cout << item->GetName() << "구매 완료!" << endl;
 	}
@@ -18,10 +19,10 @@ void Shop::BuyItem(Item* item, Character* character, Inventory* inventory)
 }
 void Shop::SellItem(Item* item, Character* character, Inventory* inventory)	//아이템과 캐릭터의 클래스를 참조
 {
-	if (inventory->GetItemCount(item) > 0)
+	if (inventory->GetItemCount(item) > 0)	//아이템 재고가 남아있을때
 	{
-		character->Setgold(character->Getgold() + item->GetPrice());		//골드 추가
-		inventory->RemoveItem(item);					//아이템 삭제
+		character->SetGold(character->GetGold() + item->GetPrice());		//현재 골드(Getgold)에 아이템 가격(GetPrice)를 더해서 Setgold의 갱신
+		inventory->RemoveItem(item);					//인벤토리에 있던 아이템 삭제
 		cout << item->GetName() << "판매 완료!" << endl;
 	}
 	else {
--- TeamProject1/Shop.h
+++ TeamProject1/Shop.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "Item.h"
-#include "Character.h"
-#include "Inventory.h"
+
+class Inventory;
+class Item;
+class Character;
 
 class Shop
 {
 private:
 	string name;	//상점 이름
-	vector<Item*> shopitems;	//아이템 리스트, 추후 아이템 클래스와 연동
-	int maxItems;	//아이템의 최대 소지 수
+	vector<Item*> shopitems;	//아이템 리스트
+	int maxitems;	//아이템의 최대 소지 수
 public:
-	Shop(string n) : name(n), maxItems(100) {}
+	Shop(string n) : name(n), maxitems(100) {}
 	void BuyItem(Item* item, Character* character, Inventory* inventory);	//아이템 구매
 	void PriceCheck(Item* item);
 	void ShowItems(Inventory* inventory);	//상품 전체 목록과 재고를 출력할뿐이라 매개변수 없음.
