18dd71f83ad351dc67c3e19685078bbee5ccaf05
RPG12165607 <sau03095@naver.com>
2025-09-08 10:45:45 +0900

    아이템 클래스 오류수정

---
+++ Item.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <iostream>
+#include "Character.h"
+
+using namespace std;
+
+class Item
+{
+
+public:
+
+	virtual string GetName() = 0;
+	virtual void Use(Character* character) = 0;
+
+};
+
+
--- TeamProject1/Item.cpp
+++ TeamProject1/Item.cpp
@@ -1,4 +1,4 @@
-#include "Item.h"
+﻿#include "Item.h"
 #include <iostream>
 #include "Character.h"
 
