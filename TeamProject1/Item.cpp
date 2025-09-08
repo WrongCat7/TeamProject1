ac45003d4be90a0bd41c54900dd2cdadfbc84a59
RPG12165607 <sau03095@naver.com>
2025-09-08 20:16:52 +0900

    ItemsName함수삭제

--- TeamProject1/Item.cpp
+++ TeamProject1/Item.cpp
@@ -2,16 +2,6 @@
 #include <iostream>
 #include "Character.h"
 
-void Item::ItemsName(int choice)
-{
-	Item* shopitems;
-	switch (choice)
-	{
-	case 0: shopitems = new HealthPotion(); break;
-	case 1: shopitems = new AttackBoost(); break;
-	}
 
-	return shopitems;
-}
 
-//주석2
+//주석1
