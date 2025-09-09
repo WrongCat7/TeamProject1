2061927f7ba366f74e2b6e3b8680b0ac31fb147a
Umsangmin <osm0991@naver.com>
2025-09-09 12:02:54 +0900

    기존 코드를 새롭게 다시 작성

--- TeamProject1/Shop.cpp
+++ TeamProject1/Shop.cpp
@@ -1,53 +1,115 @@
 #include "Shop.h"
 #include <iostream>
 #include "Character.h"
-#include "Item.h"
 #include "Inventory.h"
+#include "HealthPotion.h"
+#include "AttackBoost.h"
+using namespace std;
 
 
-void Shop::BuyItem(Item* item, Character* character, Inventory* inventory)
+Shop::Shop()
 {
-	if (character->GetGold() >= item->GetPrice())	//골드가 충분하면
+	shopitems.push_back(new HealthPotion());
+	shopitems.push_back(new AttackBoost());
+}
+Shop::~Shop()
+{
+	for (auto item : shopitems)
 	{
-		character->SetGold(character->GetGold() - item->GetPrice());	//골드 차감, 현재골드 - 아이템 가격 = 남는 골드
-		inventory->AddItem(item);					//인벤토리에 아이템 추가 
-		cout << item->GetName() << "구매 완료!" << endl;
+		delete item;
 	}
-	else {											//골드가 충분하지 않을 때
+	shopitems.clear();
+}
+
+void Shop::ShowItems(Inventory* inventory, Character* character)
+{
+	int input = 0;
+
+	while (true)
+	{
+		system("cls");
+		cout << "----- 상점 -----" << endl;
+		cout << "현재 보유 골드 : " << character->GetGold() << " 골드" << endl;
+		cout << "1. 아이템 구매" << endl;
+		cout << "2. 아이템 판매" << endl;
+		cout << "3. 상점 나가기" << endl;
+		cin >> input;
+
+		if (input == 1)
+		{
+			cout << "구매 가능한 아이템 목록" << endl;
+			for (int i = 0; i < shopitems.size(); i++)
+			{
+				cout << i << ":" << shopitems[i]->GetName()
+					<< " " << shopitems[i]->GetPrice() << " 골드" << endl;
+			}
+			int buynum;
+			cout << "번호를 입력하세요 (취소 3) : " << endl;
+			cin >> buynum;
+
+			if (buynum > 0 && buynum <= 3)
+			{
+				BuyItem(character, inventory, shopitems[buynum - 1]);
+			}
+			else
+			{
+				cout << "잘못된 입력입니다 1~4번 중에서 골라주세요." << endl;
+			}
+		}
+		else if (input == 2)
+		{
+			if (inventory->GetItemCount() == 0)	//GetItemCount 함수를 전체 아이템 수를 반환하게 바꾸면 사용가능
+			{
+				cout << "판매할 아이템이 없습니다." << endl;
+			}
+			else
+			{
+				inventory->DisplayItems();
+				int sellnum;
+				cout << "번호를 입력하세요 (취소 3) : " << endl;
+				cin >> sellnum;
+				if (sellnum > 0 && sellnum <= 3)
+				{
+					SellItem(character, inventory, shopitems[sellnum - 1]);
+				}
+			}
+		}
+		else if (input == 3)
+		{
+			return;
+		}
+		else
+		{
+			cout << "잘못된 입력입니다." << endl;
+		}
+
+		system("pause");
+	}
+}
+void Shop::BuyItem(Character* character, Inventory* inventory, Item* item)
+{
+	if (character->GetGold() >= item->GetPrice())
+	{
+		character->SetGold(character->GetGold() - item->GetPrice());
+		inventory->AddItem(item);
+		cout << item->GetName() << " 구매 완료!" << endl;
+	}
+	else
+	{
 		cout << "골드가 부족합니다!" << endl;
 	}
 }
-void Shop::SellItem(Item* item, Character* character, Inventory* inventory)	//아이템과 캐릭터의 클래스를 참조
-{
-	if (inventory->GetItemCount(item) > 0)	//아이템 재고가 남아있을때
-	{
-		character->SetGold(character->GetGold() + item->GetPrice());		//현재 골드(Getgold)에 아이템 가격(GetPrice)를 더해서 Setgold의 갱신
-		inventory->RemoveItem(item);					//인벤토리에 있던 아이템 삭제
-		cout << item->GetName() << "판매 완료!" << endl;
-	}
-	else {
-		cout << "아이템이 없습니다!" << endl;
-	}
-}
-void Shop::PriceCheck(Item* item)
-{
-	cout << item->GetName() << "가격 : " << item->GetPrice() << "골드" << endl;
-}
-int Shop::GetItemCount(Item* item, Inventory* inventory)
-{
-	return inventory->GetItemCount(item);
-}
-void Shop::ShowItems(Inventory* inventory)
-{
-	cout << "상점에 오신 것을 환영합니다!" << endl;
 
-	for (auto& item : shopitems)
+void Shop::SellItem(Character* character, Inventory* inventory, Item* item)
+{
+	if (inventory->GetItemCount(item) > 0)
 	{
-		cout << item->GetName()
-			<< " : " << item->GetPrice()
-			<< "골드"
-			<< " / 재고" << inventory->GetItemCount(item)
-			<< "개\n";
+		character->SetGold(character->GetGold() + item->GetPrice());
+		inventory->RemoveItem(item);
+		cout << item->GetName() << " 판매 완료! (+" << item->GetPrice() << " 골드)" << endl;
+	}
+	else
+	{
+		cout << "판매할 아이템이 없습니다!" << endl;
 	}
 }
-
--- TeamProject1/Shop.h
+++ TeamProject1/Shop.h
@@ -2,21 +2,19 @@
 #include <string>
 #include <vector>
 
-class Inventory;
-class Item;
 class Character;
+class Item;
+class Inventory;
 
 class Shop
 {
 private:
-	string name;	//상점 이름
-	vector<Item*> shopitems;	//아이템 리스트
-	int maxitems;	//아이템의 최대 소지 수
+	void BuyItem(Character* character, Inventory* inventory, Item* item);	//아이템 구매
+	void SellItem(Character* character, Inventory* inventory, Item* item);	//아이템 판매
+	std::vector<Item*> shopitems;	//아이템 리스트
+
 public:
-	Shop(string n) : name(n), maxitems(100) {}
-	void BuyItem(Item* item, Character* character, Inventory* inventory);	//아이템 구매
-	void PriceCheck(Item* item);
-	void ShowItems(Inventory* inventory);	//상품 전체 목록과 재고를 출력할뿐이라 매개변수 없음.
-	int GetItemCount(Item* item, Inventory* inventory);
-	void SellItem(Item* item, Character* character, Inventory* inventory);	//아이템 판매
+	Shop();
+	~Shop();
+	void ShowItems(Inventory* inventory, Character* character);
 };
