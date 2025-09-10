887d57164e0583ce32eb364750da7d68be419ff2
Umsangmin <osm0991@naver.com>
2025-09-10 17:42:32 +0900

    Melody 클래스 추가

---
+++ TeamProject1/Melody.cpp
@@ -0,0 +1,53 @@
+#include "Melody.h"
+#include <Windows.h>
+
+void Melody::PlayNote(int freq, int speed)
+{
+	Beep(freq, speed);
+}
+
+void Melody::BuySellMelody()
+{
+	int melody[] = { C4, E4, G4, C5 };
+	int speed[] = { 150, 150, 150, 300 };
+
+	for (int i = 0; i < 4; i++)
+	{
+		Beep(melody[i], speed[i]);
+	}
+}
+
+
+void Melody::ShopInBGM()
+{
+	int melody[] = { G4, A4, B4, C5, B4, A4, G4 };
+	int speed[] = { 200, 200, 200, 300, 200, 200, 200 };
+
+	for (int i = 0; i < 7; i++)
+	{
+		Beep(melody[i], speed[i]);
+	}
+
+}
+
+void Melody::ShopOutBGM()
+{
+	int melody[] = { C5, B4, A4, G4, F4, E4 };
+	int speed[] = { 200, 200, 200, 200, 200, 300 };
+
+	for (int i = 0; i < 6; i++)
+	{
+		Beep(melody[i], speed[i]);
+	}
+}
+
+void Melody::BattelBGM()
+{
+	int melody[] = { C4, E4, G4, C5, G4, E4 };
+	int speed[] = { 300, 300, 300, 400, 300, 300 };
+
+	for (int i = 0; i < 6; i++)
+	{
+		Beep(melody[i], speed[i]);
+	}
+}
---
+++ TeamProject1/Melody.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+class Melody
+{
+private:
+	const int C4 = 261;
+	const int D4 = 294;
+	const int E4 = 329;
+	const int F4 = 349;
+	const int G4 = 392;
+	const int A4 = 440;
+	const int B4 = 493;
+	const int C5 = 523;
+
+public:
+	void PlayNote(int freq, int speed);
+	void BuySellMelody();
+	void ShopInBGM();
+	void ShopOutBGM();
+	void BattelBGM();
+};
+
+
